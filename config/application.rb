76a02271bc01c51499df909834087ce65fc066e4
satoshitodaka <satoshitodaka0705@gmail.com>
2022-05-05 14:53:14 +0900

    [Add]generate時に無駄なファイルが生成されないよう設定

--- config/application.rb
+++ config/application.rb
@@ -34,5 +34,10 @@ module CookNote
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    config.generators do |g|
+      g.skip_routes true
+      g.assets false
+      g.helper false
+    end
   end
 end
