441cb971c93711def7a8ccc888313d26040ac5bf
satoshitodaka <satoshitodaka0705@gmail.com>
2022-05-07 19:19:34 +0900

    [Add]MDBをCDNで読み込むよう変更

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,3 @@
-import * as mdb from 'mdb-ui-kit'; // lib
-import { Input } from 'mdb-ui-kit'; // modul
-
 // This file is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
